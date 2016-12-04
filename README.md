ceae3edeee351947b037631dae5f438b4f5b0370
Serge Bedzhyk <smileart21@gmail.com>
2016-12-04 21:39:30 +0200

    feat: add installation script and optional git aliases

--- README.md
+++ README.md
@@ -7,23 +7,22 @@ These are **very simple** custom git commands that enforce the git user to write
 
 ## Installation:
 
-1. Clone this repo, prefferably in your $HOME directory.
-``` git clone https://github.com/fteem/git-semantic-commits ~/.git-semantic-commits ```
+1. Clone this repo, prefferably in your $HOME directory. ```git clone https://github.com/fteem/git-semantic-commits ~/.git-semantic-commits```
 
-2. Add the folder to your $PATH.
-  * if using Bash, open ```~/.bashrc``` with your favourite editor and add this line: <br/> ``` export PATH=$PATH:$HOME/.git-semantic-commits:$PATH ```
-  * if using Zsh, open ```~/.zshrc``` with your favourite editor and add this line: <br/>  ``` export PATH=$PATH:$HOME/.git-semantic-commits:$PATH ```
+2. Install it as a set of bash scripts or git aliases:
+  * bash scripts: ```cd ~/.git-semantic-commits && ./install.sh --scripts```
+  * git aliases ```cd ~/.git-semantic-commits && ./install.sh```
 
-    Tip: If you're using Cygwin, open it and type 'echo $USERPROFILE'. This will show you the location of the $HOME directory.
+  Tip: Installation script is idempotent and could by harmlessly executed multiple times. All it does is adding bash scripts to the PATH in your `~/.bashrc` / `~/.zshrc` files or adding git aliases to the `~/.gitconfig` file respectively (without any duplication).
 
-3. Save the file and source the changes in bashrc/zshrc: ```source ~/.bashrc``` or ```source ~/.zshrc```
-4. Commit away!
+3. Commit away!
 
 ## Usage
 
 There are 8 new Git commands now.
 
 New command -> what it does:
+
 * ```git feat "commit-message-here"``` -> ```git commit -m 'feat: commit-message-here'```
 * ```git docs "commit-message-here"``` -> ```git commit -m 'docs: commit-message-here'```
 * ```git chore "commit-message-here"``` -> ```git commit -m 'chore: commit-message-here'```
@@ -37,6 +36,11 @@ If you would still like to use your text editor for your commit messages
 you can omit the message, and do your commit message in your editor.
 
 * ```git feat``` -> ```git commit -m 'feat: ' -e```
+
+Aliases for those who uses [git-extras](https://github.com/tj/git-extras):
+
+* ```git rf "commit-message-here"``` -> ```git commit -m 'refactor: commit-message-here'```
+* ```git ch "commit-message-here"``` -> ```git commit -m 'chore: commit-message-here'```
 
 ## How to contribute
 Open a pull request/issue or fork this repo and submit your changes via a pull request.
---
+++ install.sh
@@ -0,0 +1,79 @@
+#!/bin/sh
+
+if [[ -n $1 ]] && [[ $1 == '--scripts' ]]; then
+  echo '💡  Installing scripts…'
+
+  PATH_LINE='export PATH=$PATH:$HOME/.git-semantic-commits:$PATH'
+  BASHRC=~/.bashrc
+  ZSHRC=~/.zshrc
+
+  if [ -f $BASHRC ]; then
+    if ! grep -Fxq "$PATH_LINE" $BASHRC
+    then
+      echo
+      echo "📝  Adding path to $BASHRC"
+      echo $PATH_LINE >> $BASHRC
+      source $BASHRC
+    fi
+  else
+    echo 'NOPE'
+  fi
+
+  if [ -f $ZSHRC ]; then
+    if ! grep -Fxq "$PATH_LINE" $ZSHRC
+    then
+      echo
+      echo "📝  Adding path to $ZSHRC"
+      echo $PATH_LINE >> $ZSHRC
+      source $ZSHRC
+    fi
+  fi
+else
+  echo '💡  Installing git aliases…'
+
+  # git-extras/chore compatibility https://goo.gl/oZUN3V
+  if ! git config --global --get-all alias.ch &>/dev/null; then
+    git config --global alias.ch '!f() { [[ -z "$GIT_PREFIX" ]] || cd "$GIT_PREFIX" && if [[ -z $1 ]]; then git commit -m "chore: " -e; else git commit -m "chore: $1"; fi }; f'
+  fi
+
+  if ! git config --global --get-all alias.chore &>/dev/null; then
+    git config --global alias.chore '!f() { [[ -z "$GIT_PREFIX" ]] || cd "$GIT_PREFIX" && if [[ -z $1 ]]; then git commit -m "chore: " -e; else git commit -m "chore: $1"; fi }; f'
+  fi
+
+  if ! git config --global --get-all alias.docs &>/dev/null; then
+    git config --global alias.docs '!f() { [[ -z "$GIT_PREFIX" ]] || cd "$GIT_PREFIX" && if [[ -z $1 ]]; then git commit -m "docs: " -e; else git commit -m "docs: $1"; fi }; f'
+  fi
+
+  if ! git config --global --get-all alias.feat &>/dev/null; then
+    git config --global alias.feat '!f() { [[ -z "$GIT_PREFIX" ]] || cd "$GIT_PREFIX" && if [[ -z $1 ]]; then git commit -m "feat: " -e; else git commit -m "feat: $1"; fi }; f'
+  fi
+
+  if ! git config --global --get-all alias.fix &>/dev/null; then
+    git config --global alias.fix '!f() { [[ -z "$GIT_PREFIX" ]] || cd "$GIT_PREFIX" && if [[ -z $1 ]]; then git commit -m "fix: " -e; else git commit -m "fix: $1"; fi }; f'
+  fi
+
+  if ! git config --global --get-all alias.localize &>/dev/null; then
+    git config --global alias.localize '!f() { [[ -z "$GIT_PREFIX" ]] || cd "$GIT_PREFIX" && if [[ -z $1 ]]; then git commit -m "localize: " -e; else git commit -m "localize: $1"; fi }; f'
+  fi
+
+  # git-extras/refactor compatibility https://goo.gl/FIvSyo
+  if ! git config --global --get-all alias.rf &>/dev/null; then
+    git config --global alias.rf '!f() { [[ -z "$GIT_PREFIX" ]] || cd "$GIT_PREFIX" && if [[ -z $1 ]]; then git commit -m "refactor: " -e; else git commit -m "refactor: $1"; fi }; f'
+  fi
+
+  if ! git config --global --get-all alias.refactor &>/dev/null; then
+    git config --global alias.refactor '!f() { [[ -z "$GIT_PREFIX" ]] || cd "$GIT_PREFIX" && if [[ -z $1 ]]; then git commit -m "refactor: " -e; else git commit -m "refactor: $1"; fi }; f'
+  fi
+
+  if ! git config --global --get-all alias.style &>/dev/null; then
+    git config --global alias.style '!f() { [[ -z "$GIT_PREFIX" ]] || cd "$GIT_PREFIX" && if [[ -z $1 ]]; then git commit -m "style: " -e; else git commit -m "style: $1"; fi }; f'
+  fi
+
+  if ! git config --global --get-all alias.test &>/dev/null; then
+    git config --global alias.test '!f() { [[ -z "$GIT_PREFIX" ]] || cd "$GIT_PREFIX" && if [[ -z $1 ]]; then git commit -m "test: " -e; else git commit -m "test: $1"; fi }; f'
+  fi
+fi
+
+echo
+echo '✅  Done! Now you can use semantic commits.'
+echo 'ℹ️  See: https://git.io/v1luX for more information'
